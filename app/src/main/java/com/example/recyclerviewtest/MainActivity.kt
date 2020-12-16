1c926a1eaf23b79fd877e8a2da26f8a4adef02f6
Kai2020-tech <kai5266@gmail.com>
2020-12-16 17:16:54 +0800

    finish item click and textView click

--- app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
@@ -2,16 +2,36 @@ package com.example.recyclerviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerviewtest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),IClick {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val a = binding.root
-        val b = R.layout.activity_main
+//        val a = binding.root
+//        val b = R.layout.activity_main
 
         binding.tvTitle.text = "Recycler View Test"
+
+        val dataList = listOf<String>(
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"
+        )
+
+        val rvAdapter = RvAdapter(this)
+        binding.rvRecyclerView.adapter = rvAdapter
+        binding.rvRecyclerView.layoutManager = LinearLayoutManager(this)
+        rvAdapter.update(dataList)
+    }
+
+    override fun onItemClick(position: Int) {
+        Toast.makeText(this, "position $position clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onItemViewClick(title: String) {
+        Toast.makeText(this, " $title clicked", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/recyclerviewtest/RvAdapter.kt
+++ app/src/main/java/com/example/recyclerviewtest/RvAdapter.kt
@@ -1,11 +1,58 @@
 package com.example.recyclerviewtest
 
-import android.view.View
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewtest.databinding.ItemModelBinding
 
-class RvAdapter(): RecyclerView.Adapter<> {
+class RvAdapter(private val clickListener: IClick) : RecyclerView.Adapter<RvAdapter.MyViewHolder>() {
+    private val innerList = mutableListOf<String>()
+    var i = 0
 
-inner class MyViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
+    inner class MyViewHolder(itemView: ItemModelBinding) : RecyclerView.ViewHolder(itemView.root){
+        var itemTitle = itemView.tvItemTitle
+        init {
+            itemView.cardView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    clickListener.onItemClick(position)
+                }
+            }
+            itemView.tvItemTitle.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    clickListener.onItemViewClick(itemView.tvItemTitle.text.toString())
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding =
+            ItemModelBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        i++
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return innerList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = innerList[position]
+        holder.itemTitle.text = currentItem
+        Log.d("viewHolder","$i")
+    }
+
+    fun update(updateList: List<String>) {
+        innerList.clear()
+        innerList.addAll(updateList)
+        this.notifyDataSetChanged()
+    }
+}
+
+interface IClick {
+    fun onItemClick(position: Int)
+    fun onItemViewClick(title:String)
 }
