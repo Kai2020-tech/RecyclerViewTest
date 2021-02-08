92d31c4ba3c428d2ab5d47a346c34a67338bfcc0
Kai2020-tech <kai5266@gmail.com>
2021-02-08 14:54:55 +0800

    init

--- app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
@@ -6,24 +6,26 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerviewtest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(),RvAdapter.IClick {
+class MainActivity : AppCompatActivity(), RvAdapter.IClick {
+    private lateinit var actManBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        actManBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(actManBinding.root)
 //        val a = binding.root
 //        val b = R.layout.activity_main
 
-        binding.tvTitle.text = "Recycler View Test"
+        actManBinding.tvTitle.text = "Recycler View Test"
 
         val dataList = listOf<RvData>(
             RvData("1"),
             RvData("2"),
             RvData("3"),
             RvData("4"),
-            RvData("5",true),
+            RvData("5", true),
             RvData("6"),
-            RvData("7",true),
+            RvData("7", true),
             RvData("8"),
             RvData("9"),
             RvData("10")
@@ -45,12 +47,12 @@ class MainActivity : AppCompatActivity(),RvAdapter.IClick {
 //        })  //第二種方式
 
 
-        binding.rvRecyclerView.adapter = rvAdapter
-        binding.rvRecyclerView.layoutManager = LinearLayoutManager(this)
+        actManBinding.rvRecyclerView.adapter = rvAdapter
+        actManBinding.rvRecyclerView.layoutManager = LinearLayoutManager(this)
         rvAdapter.update(dataList)
     }
 
-    override fun onItemClick(position: Int) {
+    override fun onItemClick(position: Int): Unit {
         Toast.makeText(this, "position $position clicked", Toast.LENGTH_SHORT).show()
     }
 
